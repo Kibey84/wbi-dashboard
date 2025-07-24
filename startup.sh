88b4ab3ca042cd78fa6fbb7bdf9e83c93dbd275c
Kibey84 <kibey84@gmail.com>
2025-07-23 21:23:07 -0400

    Added zip debug steps

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Update the package list and install the font packages required by the PDF library
-apt-get update
-apt-get install -y fonts-freefont-ttf
-
-# This command starts the Gunicorn server that runs the Flask app
-gunicorn --bind=0.0.0.0 --timeout 600 app:app
